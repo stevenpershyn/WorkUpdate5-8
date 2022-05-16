29d4dd69bdabd62248a6cddeb9fbf2a6c3981b62
stevenpershyn <102390494+stevenpershyn@users.noreply.github.com>
2022-05-16 15:47:13 -0400

    Add files via upload
    Added JavaScript practice

---
+++ test1.js
@@ -0,0 +1,26 @@
+let testVar = "hello";
+
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter(word => word.length > 6);
+
+testVar = 5;
+
+const testVar2 = "hello";
+
+const testVar3 = null;
+
+const testVar4 = true;
+
+let testArray = ['a', 'b'];
+
+let a = 78;
+let b = 36;
+console.log("Hello World");
+console.log(typeof testVar);
+console.log(typeof testVar2);
+console.log(typeof testVar3);
+console.log(typeof testVar4);
+console.log(testArray);
+console.log(b%a);
+console.log(result);
